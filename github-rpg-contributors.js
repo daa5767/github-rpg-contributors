dc026c883be9b7c96bac258a57b8cc20feb36b58
daa5767 <daa5767@psu.edu>
2025-03-18 14:11:33 -0400

    Initial commit

---
+++ github-rpg-contributors.js
@@ -0,0 +1,160 @@
+/**
+ * Copyright 2025 daa5767
+ * @license Apache-2.0, see LICENSE for full text.
+ */
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import "@haxtheweb/rpg-character/rpg-character.js";
+
+/**
+ * `github-rpg-contributors`
+ *
+ * @demo index.html
+ * @element github-rpg-contributors
+ */
+export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
+  static get tag() {
+    return "github-rpg-contributors";
+  }
+
+  constructor() {
+    super();
+    this.title = "Github Contributors";
+    this.organization = "haxtheweb";
+    this.repo = "webcomponents";
+    this.limit = 25;
+    this.items = [];
+    this.loading = false;
+
+    this.t = this.t || {};
+    this.t = {
+      ...this.t,
+      title: "Title",
+    };
+    this.registerLocalization({
+      context: this,
+      localesPath:
+        new URL("./locales/github-rpg-contributors.ar.json", import.meta.url)
+          .href + "/../",
+      locales: ["ar", "es", "hi", "zh"],
+    });
+  }
+
+  // Lit reactive properties
+  static get properties() {
+    return {
+      ...super.properties,
+      title: { type: String },
+      organization: { type: String },
+      repo: { type: String },
+      limit: { type: Number },
+      items: { type: Array },
+      loading: { type: Boolean },
+    };
+  }
+
+  // Lit scoped styles
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+        h3 span {
+          font-size: var(
+            --github-rpg-contributors-label-font-size,
+            var(--ddd-font-size-s)
+          );
+        }
+        .rpg-wrapper {
+          display: inline-flex;
+        }
+      `,
+    ];
+  }
+
+  // Lit render the HTML
+  render() {
+    return html`
+      <h2>${this.title}</h2>
+      <details open>
+        <summary>Search Github Contributors</summary>
+        <div>
+          <input
+            id="orgInput"
+            placeholder="Enter Organization"
+            @input="${this.handleOrgInput}"
+          />
+          <input
+            id="repoInput"
+            placeholder="Enter Repo"
+            @input="${this.handleRepoInput}"
+          />
+        </div>
+      </details>
+      ${this.loading ? html`<div class="loading">Loading...</div>` : ""}
+      <div class="results">
+        ${this.items.map(
+          (item) => html`
+            <rpg-character></rpg-character>
+            <div class="contributor">
+              <img src="${item.avatar_url}" width="50" height="50" />
+              <p>${item.login}</p>
+            </div>
+          `
+        )}
+      </div>
+    `;
+  }
+
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    if (
+      changedProperties.has("organization") ||
+      changedProperties.has("repo")
+    ) {
+      if (this.organization && this.repo) {
+        this.fetchContributors();
+      }
+    }
+  }
+  fetchContributors() {
+    this.loading = true;
+    console.log(
+      `https://api.github.com/repos/${this.organization}/${this.repo}/contributors?per_page=${this.limit}`
+    );
+    fetch(
+      `https://api.github.com/repos/${this.organization}/${this.repo}/contributors?per_page=${this.limit}`
+    )
+      .then((response) => (response.ok ? response.json() : []))
+      .then((data) => {
+        if (data.length > 0) {
+          this.items = [];
+          this.items = data;
+          this.loading = false;
+        }
+      });
+  }
+
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+      .href;
+  }
+}
+
+globalThis.customElements.define(
+  GithubRpgContributors.tag,
+  GithubRpgContributors
+);
